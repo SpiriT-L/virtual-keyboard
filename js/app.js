cb10ab5bdda3dc7504341f209aadc340f68355ad
LeonID <lmatskevich79@gmail.com>
2022-05-05 21:35:57 +0300

    feat: added some styles in html via js

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
+
+  properties: {
+    value: '',
+    capsLock: false,
+  },
+
+  init() {
+    this.elements.main = document.createElement('div');
+    this.elements.keysContainer = document.createElement('div');
+
+    this.elements.main.classList.add('keyboard', '1keyboard--hidden');
+    this.elements.keysContainer.classList.add('keyboard__keys');
+
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
+
+  _createKeys() {},
+
+  _triggerEvent(handlerName) {
+    console.log('Event Triggered! Event Name: ' + handlerName);
+  },
+
+  _toggleCapsLock() {
+    console.log('Caps Lock Toggled!');
+  },
+
+  open(initialValue, oninput, onclose) {},
+
+  close() {},
+};
+
+window.addEventListener('DOMContentLoaded', function () {
+  Keyboard.init();
+});
